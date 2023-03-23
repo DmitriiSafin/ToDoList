41b71682619653e005c7b70ae7cb933a82aad89f
Dmitry Safin <developersafin@gmail.com>
2023-03-23 10:38:36 +0500

    Save in FileManager

---
+++ ToDoList/Controllers/ToDoListViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Дмитрий on 20.03.2023.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    var itemArray = [Item]()
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(dataFilePath ?? "")
+        
+        loadItems()
+        setupNavigationBar()
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New ToDo Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) {
+            action in
+            
+            let newItem = Item()
+            newItem.title = textField.text ?? "New Item"
+            
+            self.itemArray.append(newItem)
+            self.saveItems()
+        }
+        
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true)
+        
+    }
+    
+    private func setupNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        navigationController?.navigationBar.tintColor = .white
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .systemCyan
+        appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+    }
+}
+
+extension ToDoListViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        itemArray[indexPath.row].done.toggle()
+        saveItems()
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    //MARK: - Model Manupulation Methods
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error \(error)")
+            }
+        }
+    }
+}
+
---
+++ ToDoList/DataModel/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  ToDoList
+//
+//  Created by Дмитрий on 22.03.2023.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
